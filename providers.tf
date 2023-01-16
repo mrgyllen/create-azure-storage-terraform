e921930fb59ca5520c4bdac524fef908c6c4187f
mrgyllen <thomas@gyllencreutz.net>
2023-01-16 21:43:17 +0100

    removed variable usage

--- providers.tf
+++ providers.tf
@@ -2,9 +2,9 @@
 terraform {
   required_version = ">= 1.3"
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.storage_container_name
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "terraformstate"
+    container_name       = "tfstate"
     key                  = "actions.tfstate"
   }
   required_providers {
