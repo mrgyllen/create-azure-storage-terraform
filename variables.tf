f4dc149efafd3a18b0ff2d227546a151cb36de73
mrgyllen <thomas@gyllencreutz.net>
2023-01-16 20:55:13 +0100

    added a few variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,35 @@
-# company
+resource "random_string" "resource_code" {
+  length = 8
+  upper = false
+  numeric = true
+  lower = true
+  special = false
+}
+
 variable "resource_group_name" {
   description   = "(Required) Specifies the Resource Group where the Azure Storage Account should exist."
   default       = "terraform-state-rg"
   type          = string
 }
 
-# environment
+variable "storage_account_name" {
+  description   = "(Required) Specifies the Azure Storage Acccount name."
+  default       = "terraformstate${random_string.resource_code.result}"
+  type          = string
+}
+
+variable "storage_container_name" {
+  description   = "(Required) Specifies the storage container name to be used for terraform state."
+  default       = "tfstate"
+  type          = string
+}
+
 variable "environment" {
   type = string
-  description = "(Required) This variable defines the environment to be built"
+  description = "(Required) This variable defines the environment tag"
   default = "Dev"
 }
 
-# azure region
 variable "location" {
   type = string
   description = "(Required) Azure region where resources will be created"
