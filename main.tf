d22e043f38e9bf40a4b4d808374661d7279f2a8d
mrgyllen <thomas@gyllencreutz.net>
2023-01-17 22:29:23 +0100

    fixed azurerm 3.0 changes

--- main.tf
+++ main.tf
@@ -14,14 +14,14 @@ resource "azurerm_resource_group" "state-rg" {
 resource "azurerm_storage_account" "state-sta" {
   depends_on = [azurerm_resource_group.state-rg]
   
-  name                      = var.storage_account_name
-  resource_group_name       = azurerm_resource_group.state-rg.name
-  location                  = azurerm_resource_group.state-rg.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  enable_https_traffic_only = true
-  allow_blob_public_access  = false
+  name                              = var.storage_account_name
+  resource_group_name               = azurerm_resource_group.state-rg.name
+  location                          = azurerm_resource_group.state-rg.location
+  account_kind                      = "StorageV2"
+  account_tier                      = "Standard"
+  account_replication_type          = "LRS"
+  enable_https_traffic_only         = true
+  allow_nested_items_to_be_public   = false
 
   lifecycle {
     prevent_destroy = true
