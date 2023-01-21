508ae8a39dcff7438e77dad35b7a616e117c098e
mrgyllen <thomas@gyllencreutz.net>
2023-01-21 22:02:39 +0100

    updated

--- main.tf
+++ main.tf
@@ -1,49 +1,94 @@
-resource "azurerm_resource_group" "state-rg" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "tfstate" {
+  name     = = var.resource_group_name == "" ? module.resource_group_label.id : var.resource_group_name
   location = var.location
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Azure Storage
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_storage_account" "tfstate" {
   
-  lifecycle {
-    prevent_destroy = true
-  }
+  name                              = var.storage_account_name == "" ? module.storage_account_label.id : var.storage_account_name
+  resource_group_name               = azurerm_resource_group.tfstate.name
+  location                          = azurerm_resource_group.tfstate.location
+  account_tier                      = var.storage_account_tier
+  account_replication_type          = var.storage_account_replication_type
+  
+  access_tier   = "Hot"
+  account_kind  = "StorageV2"
+  
+  identity {
+    type = "SystemAssigned"
+  }  
+  
+  # Security
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+  allow_blob_public_access  = false
   
   tags = {
     environment = var.environment
   }
 }
 
-resource "random_string" "output" {
-  length = 8
-  upper = false
-  numeric = true
-  lower = true
-  special = false
-}
-
-resource "azurerm_storage_account" "state-sta" {
-  depends_on = [azurerm_resource_group.state-rg]
-  
-  name                              = "terraformstate${random_string.output.result}"
-  resource_group_name               = azurerm_resource_group.state-rg.name
-  location                          = azurerm_resource_group.state-rg.location
-  account_kind                      = "StorageV2"
-  account_tier                      = "Standard"
-  account_replication_type          = "LRS"
-  enable_https_traffic_only         = true
-  allow_nested_items_to_be_public   = false
-
-  lifecycle {
-    prevent_destroy = true
-  }
-  
-  tags = {
-    environment = var.environment
-  }
-}
-
-resource "azurerm_storage_container" "core-container" {
+resource "azurerm_storage_container" "tfstate" {
   depends_on = [azurerm_storage_account.state-sta]
   
-  name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.state-sta.name
+  name                  = module.storage_container_label.id
+  storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
+}
+  
+# ---------------------------------------------------------------------------------------------------------------------
+# Key Vault
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_key_vault" "tfstate" {
+  name                     = module.key_vault_label.id
+  location                 = azurerm_resource_group.tfstate.location
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = var.key_vault_sku_name
+  purge_protection_enabled = true
+  soft_delete_enabled      = true
+}
+
+resource "azurerm_key_vault_access_policy" "storage" {
+  key_vault_id = azurerm_key_vault.tfstate.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_storage_account.tfstate.identity.0.principal_id
+
+  key_permissions    = ["get", "create", "list", "restore", "recover", "unwrapkey", "wrapkey", "purge", "encrypt", "decrypt", "sign", "verify"]
+  secret_permissions = ["get"]
+}
+
+resource "azurerm_key_vault_access_policy" "client" {
+  key_vault_id = azurerm_key_vault.tfstate.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions    = ["get", "create", "delete", "list", "restore", "recover", "unwrapkey", "wrapkey", "purge", "encrypt", "decrypt", "sign", "verify"]
+  secret_permissions = ["get"]
+}
+
+resource "azurerm_key_vault_key" "tfstate" {
+  name         = module.key_vault_label.id
+  key_vault_id = azurerm_key_vault.tfstate.id
+  key_type     = var.key_vault_key_type
+  key_size     = 2048
+  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
+
+  depends_on = [
+    azurerm_key_vault_access_policy.client,
+    azurerm_key_vault_access_policy.storage,
+  ]
+  expiration_date = var.key_vault_key_expiration_date
+}
+
+resource "azurerm_storage_account_customer_managed_key" "tfstate" {
+  storage_account_id = azurerm_storage_account.tfstate.id
+  key_vault_id       = azurerm_key_vault.tfstate.id
+  key_name           = azurerm_key_vault_key.tfstate.name
+  key_version        = null # null enables automatic key rotation
 }
