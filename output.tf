ce35e3d8e978911e760cd9038328b5ec62913cac
mrgyllen <thomas@gyllencreutz.net>
2023-01-21 22:12:51 +0100

    updated output

--- output.tf
+++ output.tf
@@ -1,6 +1,5 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # Resource Group attributes
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group#attributes-reference
 # ---------------------------------------------------------------------------------------------------------------------
 
 output "resource_group_id" {
@@ -15,7 +14,6 @@ output "resource_group_name" {
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Storage Account attributes:
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account#attributes-reference
 # ---------------------------------------------------------------------------------------------------------------------
 output "storage_account_id" {
   value       = azurerm_storage_account.tfstate.id
@@ -37,33 +35,47 @@ output "storage_account_primary_access_key" {
   description = "The primary access key for the storage account. This value is sensitive and masked from Terraform output."
   sensitive   = true
 }
-/*
-output "storage_account_secondary_access_key" {
-  value       = azurerm_storage_account.tfstate.secondary_access_key
-  description = "The secondary access key for the storage account."
-  sensitive   = true
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Storage Container attributes
+# ---------------------------------------------------------------------------------------------------------------------
+
+output "storage_container_id" {
+  value       = azurerm_storage_container.tfstate.id
+  description = "The ID of the Storage Container."
 }
 
-output "storage_account_primary_connection_string" {
-  value       = azurerm_storage_account.tfstate.primary_connection_string
-  description = "The connection string associated with the primary location."
-  sensitive   = true
+output "storage_container_has_immutability_policy" {
+  value       = azurerm_storage_container.tfstate.has_immutability_policy
+  description = "Is there an Immutability Policy configured on this Storage Container?"
 }
 
-output "storage_account_secondary_connection_string" {
-  value       = azurerm_storage_account.tfstate.secondary_connection_string
-  description = "The connection string associated with the secondary location."
-  sensitive   = true
+output "storage_container_has_legal_hold" {
+  value       = azurerm_storage_container.tfstate.has_legal_hold
+  description = "Is there a Legal Hold configured on this Storage Container?"
 }
 
-output "storage_account_primary_blob_connection_string" {
-  value       = azurerm_storage_account.tfstate.primary_blob_connection_string
-  description = "The connection string associated with the primary blob location."
-  sensitive   = true
+output "storage_container_resource_manager_id" {
+  value       = azurerm_storage_container.tfstate.resource_manager_id
+  description = "The Resource Manager ID of this Storage Container."
 }
 
-output "storage_account_secondary_blob_connection_string" {
-  value       = azurerm_storage_account.tfstate.secondary_blob_connection_string
-  description = "The connection string associated with the secondary blob location."
-  sensitive   = true
+# ---------------------------------------------------------------------------------------------------------------------
+# Key Vault Attributes
+# ---------------------------------------------------------------------------------------------------------------------
+### The Key Vault ###
+output "key_vault_id" {
+  value       = azurerm_key_vault.tfstate.id
+  description = ""
+}
+
+### the Key Vault Key ###
+output "key_vault_key_id" {
+  value       = azurerm_key_vault_key.tfstate.id
+  description = "The Key Vault Key ID"
+}
+
+output "key_vault_key_version" {
+  value       = azurerm_key_vault_key.tfstate.version
+  description = "The current version of the Key Vault Key."
 }
