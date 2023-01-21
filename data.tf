218f73cf851a3e414163d0bdb6bbc02d20153e46
mrgyllen <thomas@gyllencreutz.net>
2023-01-21 21:36:36 +0100

    initial commit

---
+++ data.tf
@@ -0,0 +1,3 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/client_config
+# Use this data source to access the configuration of the AzureRM provider.
+data "azurerm_client_config" "current" {}
