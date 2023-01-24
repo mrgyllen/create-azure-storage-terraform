f20575f330d3f0b8fa7b30f0c9171f42121f8034
Thomas Gyllencreutz <thomas@gyllencreutz.net>
2023-01-24 21:07:43 +0100

    linted

--- main.tf
+++ main.tf
@@ -8,57 +8,66 @@ resource "azurerm_resource_group" "tfstate" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "azurerm_storage_account" "tfstate" {
-  
-  name                              = var.storage_account_name == "" ? module.storage_account_label.id : var.storage_account_name
-  resource_group_name               = azurerm_resource_group.tfstate.name
-  location                          = azurerm_resource_group.tfstate.location
-  account_tier                      = var.storage_account_tier
-  account_replication_type          = var.storage_account_replication_type
-  
-  access_tier   = "Hot"
-  account_kind  = "StorageV2"
-  
+
+  name                     = var.storage_account_name == "" ? module.storage_account_label.id : var.storage_account_name
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+
+  access_tier  = "Hot"
+  account_kind = "StorageV2"
+
   identity {
     type = "SystemAssigned"
-  }  
-  
+  }
+
   # Security
   enable_https_traffic_only       = true
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
-  
+
   tags = {
     environment = var.environment
   }
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  
+
   name                  = module.storage_container_label.id
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
-  
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Key Vault
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "azurerm_key_vault" "tfstate" {
-  name                     = module.key_vault_label.id
-  location                 = azurerm_resource_group.tfstate.location
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  sku_name                 = var.key_vault_sku_name
-  purge_protection_enabled = true
+  name                       = module.key_vault_label.id
+  location                   = azurerm_resource_group.tfstate.location
+  resource_group_name        = azurerm_resource_group.tfstate.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = var.key_vault_sku_name
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = true
+
+  network_acls {
+    # Allows all azure services to access your keyvault. Can be set to 'None'
+    bypass = "AzureServices"
+
+    # The Default Action to use
+    default_action = "Deny"
+  }
 }
 
 resource "azurerm_key_vault_access_policy" "storage" {
   key_vault_id = azurerm_key_vault.tfstate.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_storage_account.tfstate.identity.0.principal_id
+  object_id    = azurerm_storage_account.tfstate.identity[0].principal_id
 
-  key_permissions    = ["Get", "Create", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify",]
-  secret_permissions = ["Get",]
+  key_permissions    = ["Get", "Create", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify", ]
+  secret_permissions = ["Get", ]
 }
 
 resource "azurerm_key_vault_access_policy" "client" {
@@ -66,8 +75,8 @@ resource "azurerm_key_vault_access_policy" "client" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
-  key_permissions    = ["Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify",]
-  secret_permissions = ["Get",]
+  key_permissions    = ["Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify", ]
+  secret_permissions = ["Get", ]
 }
 
 resource "azurerm_key_vault_key" "tfstate" {
@@ -75,7 +84,7 @@ resource "azurerm_key_vault_key" "tfstate" {
   key_vault_id = azurerm_key_vault.tfstate.id
   key_type     = var.key_vault_key_type
   key_size     = 2048
-  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey",]
+  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey", ]
 
   depends_on = [
     azurerm_key_vault_access_policy.client,
